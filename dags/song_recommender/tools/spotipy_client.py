1c19e6a10b0c46716ec8986e44d35fa2494e9f62
shteken <barshteken@gmail.com>
2023-08-13 14:16:35 +0200

    add dotenv and start creating tests

--- dags/song_recommender/tools/spotipy_client.py
+++ dags/song_recommender/tools/spotipy_client.py
@@ -1,7 +1,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
 
-
+load_dotenv()
 auth_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
---
+++ tests/dags/song_recommendor/test_spotipy_client.py
@@ -0,0 +1,10 @@
+import pytest
+
+from song_recommender.dags.song_recommender.tools.spotipy_client import send_requests_to_spotify
+
+@pytest.fixture
+def api_call():
+    return api_call_example
+
+
+# def test_send_requests_to_spotify()
